7a065b68f3dca9ec407895f027e47a3c78ce518e
Aydin Halimi <aydin_halimi1@baylor.edu>
2022-03-02 15:48:48 -0600

    Midterm

--- CourseProgram.py
+++ CourseProgram.py
@@ -8,7 +8,7 @@ You have been given partial code. The objective is to reproduce the output as sh
    and the output should reflect that as shown in the picture.
 '''
 
-
+import CourseClass as c
 
 def main():
 
@@ -18,6 +18,62 @@ def main():
     status = 'open'
     students = ['John','James','Jill','Jack','Joanne']
 
+    course = c.Course(name,crn,seats,status)
+    course.get__name()
+    course.get__crn()
+    course.get__seats()
+    course.get__status()
+    
+    for students in John:
+      John = c.Register(name,crn)
+      John.get_studentname()
+      John.get_crn()
+
+    for students in James:
+      James = c.Register(name,crn)
+      James.get_studentname()
+      James.get_crn()
+
+    for students in Jill:
+      Jill = c.Register(name,crn)
+      Jill.get_studentname()
+      Jill.get_crn()
+
+    for students in Jack:
+      Jack = c.Register(name,crn)
+      Jack.get_studentname()
+      Jack.get_crn()
+
+    for students in Joanne:
+      Joanne = c.Register(name,crn)
+      Joanne.get_studentname()
+      Joanne.get_crn()
+
+
+    print("Student Name:",course.get_studentname())
+    print("Course Name:",course.get_name())
+    print("CRN:",course.get_crn())
+    print("Seats left:",course.get_status())
+
+    print("Student Name:",course.get_studentname())
+    print("Course Name:",course.get_name())
+    print("CRN:",course.get_crn())
+    print("Seats left:",course.get_status())
+
+    print("Student Name:",course.get_studentname())
+    print("Course Name:",course.get_name())
+    print("CRN:",course.get_crn())
+    print("Seats left:",course.get_status())
+
+    print("Student Name:",course.get_studentname())
+    print("Course Name:",course.get_name())
+    print("CRN:",course.get_crn())
+    print("Seats left:",course.get_status())
+
+
+    if course.get__seats < len(students):
+       print("Sorry",get_studentname,",","registration is closed for MIS 4322 - Advanced Python")
+ 
     
 main()
 
